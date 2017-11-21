2f0ee6d31beff506d502b3aafec0adece30e9da0
Loeve <1872197055@qq.com>
2017-11-21 09:59:46 +0800

    add files

---
+++ README.m
@@ -0,0 +1,22 @@
+addpath(genpath(pwd));
+% vl_setup
+
+[fname,pname]=uigetfile('*.bmp;*.png;*.jpg','pick picture file','MultiSelect', 'on');
+str = fullfile(pname,fname);
+image=imread(str);
+image = rescale_max_size(image, 200);
+fprintf('Calculating saliency map£º%d\n',k); tic
+
+% Compute features.
+[contrast_map,center_surround_map,color_spatial_map] = ...
+    extract_salient_feature_maps(image);
+% Infer saliency mask.
+ [salient_map,rects] = infer_salient_map(contrast_map, ...
+    center_surround_map,color_spatial_map);
+
+showresult(pname,filename{k},rects,image,salient_map, ...
+    contrast_map,center_surround_map,color_spatial_map);
+
+ 
+fprintf('Computation took %f\n', toc);
+
